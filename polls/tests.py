0e95c95e0bd70e6a5afc2b1df37784a2dbb179c2
kz <kick.bybe@gmail.com>
2026-02-21 21:21:25 +0900

    Add from valodation tests

--- polls/tests.py
+++ polls/tests.py
@@ -1,58 +1,17 @@
-import datetime
-from django.utils import timezone
 from django.test import TestCase
-from django.urls import reverse
-
-from .models import Question
+from polls.forms import QuestionForm
 
 
-def create_question(question_text, days):
-    """
-    days がマイナス → 過去
-    days がプラス → 未来
-    """
-    time = timezone.now() + datetime.timedelta(days=days)
-    return Question.objects.create(question_text=question_text, pub_date=time)
+class QuestionFormTest(TestCase):
+    def test_mileage_must_be_zero_or_more(self):
+        form = QuestionForm(data={
+            "question_text": "テスト点検",
+            "mileage": -1,
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn("mileage", form.errors)
 
-
-class QuestionIndexViewTests(TestCase):
-    def test_no_questions(self):
-        response = self.client.get(reverse("polls:index"))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "質問がありません。")
-        self.assertQuerySetEqual(response.context["latest_question_list"], [])
-
-    def test_past_question(self):
-        create_question("過去の質問", days=-30)
-        response = self.client.get(reverse("polls:index"))
-        self.assertQuerySetEqual(
-            response.context["latest_question_list"],
-            ["<Question: 過去の質問>"],
-            transform=repr
-        )
-
-    def test_future_question(self):
-        create_question("未来の質問", days=30)
-        response = self.client.get(reverse("polls:index"))
-        self.assertContains(response, "質問がありません。")
-        self.assertQuerySetEqual(response.context["latest_question_list"], [])
-
-    def test_future_and_past_question(self):
-        create_question("過去の質問", days=-30)
-        create_question("未来の質問", days=30)
-        response = self.client.get(reverse("polls:index"))
-        self.assertQuerySetEqual(
-            response.context["latest_question_list"],
-            ["<Question: 過去の質問>"],
-            transform=repr,
-        )
-
-    def test_two_past_questions(self):
-        create_question("過去の質問1", days=-30)
-        create_question("過去の質問2", days=-5)
-        response = self.client.get(reverse("polls:index"))
-        self.assertQuerySetEqual(
-            response.context["latest_question_list"],
-            ["<Question: 過去の質問2>", "<Question: 過去の質問1>"],
-            transform=repr,
-        )
+    def test_required_fields(self):
+        form = QuestionForm(data={})
+        self.assertFalse(form.is_valid())
+        self.assertIn("question_text", form.errors)
